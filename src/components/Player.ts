989f6dd66140e851d3437b1674b2b09dfd037eed
devrajneupane <asminneupane79@gmail.com>
2024-06-19 11:39:20 +0545

    feat(map): add horizontal movement of Map based on player position

--- src/components/Player.ts
+++ src/components/Player.ts
@@ -2,13 +2,8 @@ import { CANVAS } from "../constants/canvas";
 import { MAP } from "../constants/map";
 import { DIRECTION_MAP } from "../constants/sprites";
 import { keys } from "../input/input";
-import { Layout } from "./Layout";
-import playerSprite from "/game/sprites/player/walking.png";
-
-type Camera = {
-  x: number;
-  y: number;
-};
+import { images } from "../image/preload";
+import { MapData } from "./Layout";
 
 export default class Player {
   x: number;
@@ -18,22 +13,23 @@ export default class Player {
   sx: number;
   sy: number;
   direction: string;
-  directions: Array<string>;
+  directions: string[];
   spriteCounter: number;
   img: HTMLImageElement;
   elaspedFrame: number = 0;
   currentFrame: number = 0;
-  frameIndexes: Array<number>;
+  frameIndexes: number[];
   speed: number;
+  offsetX: number;
+  offsetY: number;
+  mapData: MapData;
   ctx: CanvasRenderingContext2D;
-  camera: Camera;
-  constructor(
-    ctx: CanvasRenderingContext2D,
-  ) {
+
+  constructor(mapData: MapData, ctx: CanvasRenderingContext2D) {
     this.x = 0;
     this.y = 0;
-    this.width = MAP.tile.size - 2;
-    this.height = MAP.tile.size - 2;
+    this.width = MAP.tile.size;
+    this.height = MAP.tile.size;
     this.ctx = ctx;
 
     this.sx = 0;
@@ -41,23 +37,20 @@ export default class Player {
 
     this.spriteCounter = 0;
     this.speed = 5;
-    this.img = new Image();
-    this.img.src = playerSprite;
+    this.img = images.player.playerSprite;
 
     this.directions = ["left", "right", "up", "down", "idle"];
     this.direction = this.directions[0];
     this.frameIndexes = DIRECTION_MAP[this.direction];
-    this.camera = {
-      x: this.x,
-      y: this.y,
-    };
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.mapData = mapData;
   }
 
   /**
    * Draw player on canvas
    */
-  // draw(map: Layout) {
-  draw(layout: Layout) {
+  draw() {
     this.elaspedFrame++;
     this.currentFrame = (this.currentFrame + 1) % this.frameIndexes.length;
 
@@ -65,17 +58,10 @@ export default class Player {
       this.direction = this.directions[0];
       this.frameBuffer();
       this.moveLeft();
-      // if (this.x < CANVAS.width / 2) {
-      //   layout.x += MAP.tile.size;
-      // }
     } else if (keys.right) {
       this.direction = this.directions[1];
       this.frameBuffer();
       this.moveRight();
-      if (this.x > CANVAS.width / 2) {
-        layout.x -= this.speed;
-        // layout.x = layout.x;
-      }
     } else if (keys.up) {
       this.direction = this.directions[2];
       this.frameBuffer();
@@ -97,8 +83,6 @@ export default class Player {
       this.width,
       this.height,
     );
-    // this.ctx.strokeStyle = "#fff";
-    // this.ctx.strokeRect(this.x, this.y, this.width, this.height);
   }
 
   /**
@@ -120,6 +104,10 @@ export default class Player {
     if (this.x < 0) {
       this.x = 0;
     }
+    if (this.x < CANVAS.width / 2 && this.offsetX < 0) {
+      this.ctx.translate(this.speed, 0);
+      this.offsetX += this.speed;
+    }
   }
 
   /**
@@ -127,8 +115,13 @@ export default class Player {
    */
   moveRight() {
     this.x += this.speed;
-    if (this.x + this.width > CANVAS.width) {
-      this.x = CANVAS.width - this.width;
+    if (this.x + this.width > this.mapData.width * MAP.tile.size) {
+      this.x = this.mapData.width * MAP.tile.size - this.width;
+    }
+    // TODO: Do not hardcode value, make `this.offsetX` dpends on width of `mapData`
+    if (this.x > CANVAS.width / 2 && this.offsetX > -460) {
+      this.ctx.translate(-this.speed, 0);
+      this.offsetX -= this.speed;
     }
   }
 
--- src/main.ts
+++ src/main.ts
@@ -12,19 +12,19 @@ const ctx = canvas.getContext("2d")!;
 canvas.width = CANVAS.width;
 canvas.height = CANVAS.height;
 
-const player = new Player(ctx);
+const player = new Player(mapData, ctx);
 const layout = new Layout(mapData, ctx);
 const layoutEditor = new LayoutEditor(mapData, canvas, ctx);
 
 function animate() {
   ctx.fillStyle = "#1f8b00";
-  ctx.clearRect(0, 0, CANVAS.width, CANVAS.height);
-  ctx.fillRect(0, 0, CANVAS.width, CANVAS.height);
+  ctx.clearRect(player.offsetX, 0, CANVAS.width, CANVAS.height);
+  ctx.fillRect(-player.offsetX, 0, CANVAS.width, CANVAS.height);
 
   layout.renderMap(player);
   layoutEditor.drawGrid();
   layoutEditor.itemBar();
-  // player.draw(layout);
+  player.draw();
   requestAnimationFrame(animate);
 }
 
